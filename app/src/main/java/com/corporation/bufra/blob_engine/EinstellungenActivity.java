da75f59ecf247b712431397baf1be44a47183cac
johansolo <johnny197@web.de>
2015-08-06 23:36:43 +0200

    BlobActivity raus. Einstellungen in Startactivity vorgezogen.

--- app/src/main/java/com/corporation/bufra/blob_engine/EinstellungenActivity.java
+++ app/src/main/java/com/corporation/bufra/blob_engine/EinstellungenActivity.java
@@ -14,6 +14,7 @@ import com.corporation.bufra.blob_engine.OpenGL.shapes.Triangle;
  * Created by Clemens on 01.08.2015.
  */
 public class EinstellungenActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+    static private String preferenceString;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -27,23 +28,27 @@ public class EinstellungenActivity extends PreferenceActivity implements Prefere
         //SharedPreferences
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String preferenceString = sharedPrefs.getString(colorPref.getKey(), "");
+        preferenceString = sharedPrefs.getString(colorPref.getKey(), "");
         onPreferenceChange(colorPref, preferenceString);
+    }
+
+    static public int getColor(){
+        return Integer.valueOf(preferenceString);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object value){
         switch (Integer.valueOf(value.toString())){
-            case 1:
-                MainActivity.getRenderer().setColor(0.8f, 0.0f, 0.0f, 1.0f);
+            case 1: //Rot
+                MainActivity.getRenderer().setColor(0.5f, 0.0f, 0.0f, 1.0f);
                 break;
-            case 2:
-                MainActivity.getRenderer().setColor(0.0f, 1.0f, 0.0f, 1.0f);
+            case 2: //Grün
+                MainActivity.getRenderer().setColor(0.0f, 0.5f, 0.0f, 1.0f);
                 break;
-            case 3:
-                MainActivity.getRenderer().setColor(0.0f, 0.0f, 1.0f, 1.0f);
+            case 3: //Blau
+                MainActivity.getRenderer().setColor(0.0f, 0.0f, 0.5f, 1.0f);
                 break;
-            case 4:
+            case 4: //Kackbraun
                 MainActivity.getRenderer().setColor(0.36f, 0.15f, 0.0f, 1.0f);
                 break;
         }
--- app/src/main/java/com/corporation/bufra/blob_engine/FullscreenActivity.java
+++ app/src/main/java/com/corporation/bufra/blob_engine/FullscreenActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -53,22 +55,13 @@ public class FullscreenActivity extends Activity {
      */
     private SystemUiHider mSystemUiHider;
 
-    Button derKnopf, oButton;
+    Button oButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fullscreen);
-
-        //Button Engine start:
-        derKnopf = (Button)findViewById(R.id.button1);
-        derKnopf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(FullscreenActivity.this, BlobActivity.class));
-            }
-        });
 
         //OpenGL start
         oButton = (Button) findViewById(R.id.button);
@@ -185,5 +178,29 @@ public class FullscreenActivity extends Activity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    //Optionsmenü
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, EinstellungenActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
